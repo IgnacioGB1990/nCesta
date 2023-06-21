b54ce802c3863e9985661b7d5b05d845fcbcc2ad
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2023-06-21 17:16:06 +0200

    added basic layout

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+
+from streamlit_lottie import st_lottie
+from streamlit_option_menu import option_menu
+
+
+st.set_page_config(page_title="nCesta",page_icon=":tada",layout="wide")
+
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title="",
+        options=["","Page 2","Page 3","Page 4","Page 5"],
+        icons=['house',"filter", 'graph-up', 'upload',"laptop"],
+        menu_icon="cast",
+        default_index=1,
+    )
+
+if selected == "":
+    st.title('Home Page')
+
+if selected =="Page 2":
+    st.title('Page 2')
+
+
+if selected =="Page 3":
+    st.title('Page 3')
+
+if selected =="Page 4":
+    st.title('Page 4')
+
+
+if selected =="Page 5":
+    st.title('Page 5')
